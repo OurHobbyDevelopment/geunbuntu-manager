974f3407e3ab285adce14d809bf545ce0d8f9334
diaa-woo <dev.dntjr0501@gmail.com>
2023-12-05 19:16:08 +0900

    FEAT :: Add common used color class
    What? : Common Color class

---
+++ src/gui/lib/common/common.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommonColor {
+  static Color navy = const Color(0xFF0E21A0);
+  static Color deepPurple = const Color(0xFF4D2DB7);
+  static Color purple = const Color(0xFF9D44C0);
+  static Color pink = const Color(0xFFEC53B0);
+}
+
+// Color table: https://colorhunt.co/palette/0e21a04d2db79d44c0ec53b0
